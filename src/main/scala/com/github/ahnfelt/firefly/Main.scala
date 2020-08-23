24f0082981168a984e2881963ac0a826c2e04b7f
ahnfelt <joakim.ahnfelt@gmail.com>
2020-08-24 00:11:22 +0200

    Emitter

--- src/main/scala/com/github/ahnfelt/firefly/Main.scala
+++ src/main/scala/com/github/ahnfelt/firefly/Main.scala
@@ -1,5 +1,7 @@
 package com.github.ahnfelt.firefly
 
+import java.io.{File, FileWriter}
+
 import com.github.ahnfelt.firefly.language.{Emitter, Parser, Tokenizer}
 
 import scala.io.Source
@@ -8,25 +10,54 @@ object Main {
 
     def main(arguments : Array[String]) : Unit = {
 
-        val file = arguments(0)
-        val source = Source.fromFile(file, "UTF-8")
+        val Array(inputPath, outputPath) = arguments
+
+        val outputFile = new File(outputPath)
+        if(outputFile.exists()) deleteDirectory(outputFile)
+        outputFile.mkdir()
+
+        for(file <- new File(inputPath).listFiles()) {
+            if(file.getName.endsWith(".ff")) {
+                compileFile(file.getAbsolutePath, new File(outputFile, file.getName.dropRight(3) + ".scala"))
+            }
+        }
+
+        println("Done.")
+
+    }
+
+    def compileFile(input : String, output : File) : Unit = {
+
+        val source = Source.fromFile(input, "UTF-8")
         val code = source.mkString
         source.close()
 
-        val tokens = Tokenizer.tokenize(file, code)
-        for(token <- tokens) println(token.kind)
+        val tokens = Tokenizer.tokenize(input, code)
+        //for(token <- tokens) println(token.kind)
 
-        println()
+        //println()
 
-        val module = new Parser(file, tokens).parseModule()
+        val module = new Parser(input, tokens).parseModule()
 
-        println(module)
-        println()
+        //println(module)
+        //println()
 
         val out = new Emitter().emitModule(module)
 
-        println(out)
+        val writer = new FileWriter(output)
+        writer.write(out)
+        writer.close()
 
+        //println(out)
+
+    }
+
+    def deleteDirectory(outputFile : File) : Unit = {
+        for(file <- outputFile.listFiles()) {
+            if(file.isDirectory) deleteDirectory(file)
+            else file.delete()
+        }
+        outputFile.delete()
     }
 
 }
