bfd99ea4fae7ff3d6d3f475eb561a4dd5ee4dcb4
ahnfelt <joakim.ahnfelt@gmail.com>
2021-12-12 21:19:20 +0100

    Generic.ff

---
+++ output/js/ff/core/Show.mjs
@@ -0,0 +1,61 @@
+import * as ff_core_Array from "../../ff/core/Array.mjs"
+
+import * as ff_core_ArrayBuilder from "../../ff/core/ArrayBuilder.mjs"
+
+import * as ff_core_Bool from "../../ff/core/Bool.mjs"
+
+import * as ff_core_Char from "../../ff/core/Char.mjs"
+
+import * as ff_core_Core from "../../ff/core/Core.mjs"
+
+import * as ff_core_Duration from "../../ff/core/Duration.mjs"
+
+import * as ff_core_Error from "../../ff/core/Error.mjs"
+
+import * as ff_core_FileSystem from "../../ff/core/FileSystem.mjs"
+
+import * as ff_core_Float from "../../ff/core/Float.mjs"
+
+import * as ff_core_Instant from "../../ff/core/Instant.mjs"
+
+import * as ff_core_Int from "../../ff/core/Int.mjs"
+
+import * as ff_core_List from "../../ff/core/List.mjs"
+
+import * as ff_core_Log from "../../ff/core/Log.mjs"
+
+import * as ff_core_Map from "../../ff/core/Map.mjs"
+
+import * as ff_core_Nothing from "../../ff/core/Nothing.mjs"
+
+import * as ff_core_Option from "../../ff/core/Option.mjs"
+
+import * as ff_core_Ordering from "../../ff/core/Ordering.mjs"
+
+import * as ff_core_Pair from "../../ff/core/Pair.mjs"
+
+import * as ff_core_Set from "../../ff/core/Set.mjs"
+
+import * as ff_core_Show from "../../ff/core/Show.mjs"
+
+import * as ff_core_String from "../../ff/core/String.mjs"
+
+import * as ff_core_System from "../../ff/core/System.mjs"
+
+import * as ff_core_TimeSystem from "../../ff/core/TimeSystem.mjs"
+
+import * as ff_core_Try from "../../ff/core/Try.mjs"
+
+import * as ff_core_Unit from "../../ff/core/Unit.mjs"
+
+
+
+
+
+export function foobar_() {
+ff_core_Show.show_(ff_core_List.Link(42, ff_core_List.Empty())/* Dictionaries: ff:core/Show.Show_ff:core/List.List(ff:core/Show.Show_ff:core/Int.Int) */)
+}
+
+
+
+
