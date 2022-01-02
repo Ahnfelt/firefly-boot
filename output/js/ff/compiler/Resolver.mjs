2316c1e9e56abbe5a96d573624b314cf2b4fa888
ahnfelt <joakim.ahnfelt@gmail.com>
2022-01-02 15:55:07 +0100

    Chaned the syntax for copying variants to Point(...p, y = 42)

--- output/js/ff/compiler/Resolver.mjs
+++ output/js/ff/compiler/Resolver.mjs
@@ -532,14 +532,7 @@ const e_ = term_a
 if((e_.index_ == 0)) {
 ff_compiler_Resolver.fail_(e_.at_, "Unbound wildcard")
 }
-{
-const _1 = e_
-{
-const _c = _1
-return ff_compiler_Syntax.EWildcard(_c.at_, _c.index_)
-return
-}
-}
+return term_
 return
 }
 }
