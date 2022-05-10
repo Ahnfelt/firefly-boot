e458e86a08de7b6e00a235dc42e0a3ebc09a27e2
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2022-05-10 12:50:44 +0200

    NodeSystem

---
+++ output/js/ff/core/NodeSystem.mjs
@@ -0,0 +1,134 @@
+
+
+import * as ff_core_Array from "../../ff/core/Array.mjs"
+
+import * as ff_core_ArrayBuilder from "../../ff/core/ArrayBuilder.mjs"
+
+import * as ff_core_Bool from "../../ff/core/Bool.mjs"
+
+import * as ff_core_BrowserSystem from "../../ff/core/BrowserSystem.mjs"
+
+import * as ff_core_Buffer from "../../ff/core/Buffer.mjs"
+
+import * as ff_core_Channel from "../../ff/core/Channel.mjs"
+
+import * as ff_core_Char from "../../ff/core/Char.mjs"
+
+import * as ff_core_Core from "../../ff/core/Core.mjs"
+
+import * as ff_core_Duration from "../../ff/core/Duration.mjs"
+
+import * as ff_core_Error from "../../ff/core/Error.mjs"
+
+import * as ff_core_FetchSystem from "../../ff/core/FetchSystem.mjs"
+
+import * as ff_core_FileHandle from "../../ff/core/FileHandle.mjs"
+
+import * as ff_core_FileSystem from "../../ff/core/FileSystem.mjs"
+
+import * as ff_core_Float from "../../ff/core/Float.mjs"
+
+import * as ff_core_HttpServer from "../../ff/core/HttpServer.mjs"
+
+import * as ff_core_Instant from "../../ff/core/Instant.mjs"
+
+import * as ff_core_Int from "../../ff/core/Int.mjs"
+
+import * as ff_core_Iterator from "../../ff/core/Iterator.mjs"
+
+import * as ff_core_JsSystem from "../../ff/core/JsSystem.mjs"
+
+import * as ff_core_JsValue from "../../ff/core/JsValue.mjs"
+
+import * as ff_core_List from "../../ff/core/List.mjs"
+
+import * as ff_core_Log from "../../ff/core/Log.mjs"
+
+import * as ff_core_Map from "../../ff/core/Map.mjs"
+
+import * as ff_core_NodeSystem from "../../ff/core/NodeSystem.mjs"
+
+import * as ff_core_Nothing from "../../ff/core/Nothing.mjs"
+
+import * as ff_core_Option from "../../ff/core/Option.mjs"
+
+import * as ff_core_Ordering from "../../ff/core/Ordering.mjs"
+
+import * as ff_core_Pair from "../../ff/core/Pair.mjs"
+
+import * as ff_core_Set from "../../ff/core/Set.mjs"
+
+import * as ff_core_Show from "../../ff/core/Show.mjs"
+
+import * as ff_core_Stream from "../../ff/core/Stream.mjs"
+
+import * as ff_core_String from "../../ff/core/String.mjs"
+
+import * as ff_core_TaskSystem from "../../ff/core/TaskSystem.mjs"
+
+import * as ff_core_TimeSystem from "../../ff/core/TimeSystem.mjs"
+
+import * as ff_core_Try from "../../ff/core/Try.mjs"
+
+import * as ff_core_Unit from "../../ff/core/Unit.mjs"
+
+// type NodeSystem
+
+
+
+
+
+
+
+
+export function NodeSystem_arguments(self_) {
+throw new Error('Function NodeSystem_arguments is missing on this target in sync context.');
+}
+
+export function NodeSystem_files(self_) {
+throw new Error('Function NodeSystem_files is missing on this target in sync context.');
+}
+
+export function NodeSystem_fetch(self_) {
+throw new Error('Function NodeSystem_fetch is missing on this target in sync context.');
+}
+
+export function NodeSystem_tasks(self_) {
+throw new Error('Function NodeSystem_tasks is missing on this target in sync context.');
+}
+
+export function NodeSystem_time(self_) {
+throw new Error('Function NodeSystem_time is missing on this target in sync context.');
+}
+
+export function NodeSystem_js(self_) {
+throw new Error('Function NodeSystem_js is missing on this target in sync context.');
+}
+
+export async function NodeSystem_arguments$(self_, $c) {
+return ff_core_Array.Array_toList(self_.array_)
+}
+
+export async function NodeSystem_files$(self_, $c) {
+return null
+}
+
+export async function NodeSystem_fetch$(self_, $c) {
+return null
+}
+
+export async function NodeSystem_tasks$(self_, $c) {
+return null
+}
+
+export async function NodeSystem_time$(self_, $c) {
+return null
+}
+
+export async function NodeSystem_js$(self_, $c) {
+return typeof globalThis !== 'undefined' ? globalThis : window
+}
+
+
+
+
