7c7a18bfb5138227937b23e4e62d3e238f16a0ed
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2022-05-10 23:20:01 +0200

    The RhymeApp can now be compiled and run with one command: ../../firefly.sh browser:node:run RhymeApp.ff

--- firefly.sh
+++ firefly.sh
@@ -7,8 +7,19 @@ p=`pwd`
 firefly=`dirname "$0"`
 mkdir -p .firefly
 
-(
-	cd "$firefly" &&
-	node output/js/ff/compiler/Main.mjs "$target" script:script "$m" "script:script@$p",ff:core@core "$p/.firefly/temporary" "$p/.firefly/output"
-)# &&
-#node ".firefly/output/script/script/$m.mjs"
+if [ "$target" == "run" ]; then
+  node --experimental-fetch ".firefly/output/node/script/script/$m.mjs"
+elif [[ "$target" == *":"* ]]; then
+  targets=$(echo "$target" | tr ":" "\n")
+  set -e
+  for t in $targets; do
+    "$0" "$t" "$2"
+  done
+else
+  (
+  	cd "$firefly" && (
+  	  [[ -d "$p/.firefly/output" ]] || mkdir "$p/.firefly/output"
+  	  node output/js/ff/compiler/Main.mjs "$target" script:script "$m" "script:script@$p",ff:core@core "$p/.firefly/temporary" "$p/.firefly/output/$target"
+    )
+  )
+fi
