67a44e0ad2b2cde80cb3588333215b374c122332
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2022-05-11 12:47:37 +0200

    Some notes on what the command line should look like

--- README.md
+++ README.md
@@ -11,3 +11,34 @@ Status: Firefly has been bootstrapped and now transpiles itself into JavaScript.
 node output/js/ff/compiler/Main.mjs node ff:compiler Main ff:compiler@compiler,ff:core@core output/temporary output/js
 ```
 
+## Future plan for the command line
+
+### Running a main file
+
+```
+firefly MyApp.ff
+```
+
+The main file may include main functions for multiple targets, such as `browserMain` and `nodeMain`. 
+Each of those targets will be built before running. 
+
+Missing dependencies are fetched from the central or configured package repository before building and running.
+
+### Building an executable
+
+```
+firefly linux MyApp.ff
+```
+
+This will generate a stand-alone executable file `MyApp` with the necessary assets and dependencies, compatible with most linux distributions. 
+
+Available platforms: `windows`, `linux`, `macos`.
+
+### Building for the browser
+
+```
+firefly browser MyApp.ff
+```
+
+This generates a single, minified `MyApp.js`, compatible with all modern browsers (ES6+).
+
--- output/js/ff/compiler/Main.mjs
+++ output/js/ff/compiler/Main.mjs
@@ -90,7 +90,7 @@ import * as ff_core_Unit from "../../ff/core/Unit.mjs"
 
 
 
-export function nodeMain_(system_) {
+export function main_(system_) {
 const target_ = ff_core_List.List_expect(ff_core_NodeSystem.NodeSystem_arguments(system_), 0);
 const mainPackage_ = ff_core_List.List_expect(ff_core_NodeSystem.NodeSystem_arguments(system_), 1);
 const mainModule_ = ff_core_List.List_expect(ff_core_NodeSystem.NodeSystem_arguments(system_), 2);
@@ -141,7 +141,7 @@ return ff_core_Pair.Pair(ff_core_Array.Array_expect(parts_, 0), ff_core_Array.Ar
 })), ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String)
 }
 
-export async function nodeMain_$(system_, $c) {
+export async function main_$(system_, $c) {
 const target_ = ff_core_List.List_expect((await ff_core_NodeSystem.NodeSystem_arguments$(system_, $c)), 0);
 const mainPackage_ = ff_core_List.List_expect((await ff_core_NodeSystem.NodeSystem_arguments$(system_, $c)), 1);
 const mainModule_ = ff_core_List.List_expect((await ff_core_NodeSystem.NodeSystem_arguments$(system_, $c)), 2);
@@ -201,7 +201,7 @@ const controller = new AbortController()
 controller.promises = new Set()
 let interval = setInterval(() => {}, 24 * 60 * 60 * 1000)
 try {
-await nodeMain_$({array_: typeof process !== 'undefined' ? process.argv.slice(2) : []}, controller)
+await main_$({array_: typeof process !== 'undefined' ? process.argv.slice(2) : []}, controller)
 } finally {
 controller.abort()
 clearInterval(interval)
