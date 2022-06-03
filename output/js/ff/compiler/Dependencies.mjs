b1a492d84cedb5085d2f21f2549e5694d4538be5
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2022-06-03 12:35:51 +0200

    Dependency resolution

---
+++ output/js/ff/compiler/Dependencies.mjs
@@ -0,0 +1,196 @@
+
+
+import * as ff_compiler_Dependencies from "../../ff/compiler/Dependencies.mjs"
+
+import * as ff_compiler_Parser from "../../ff/compiler/Parser.mjs"
+
+import * as ff_compiler_Syntax from "../../ff/compiler/Syntax.mjs"
+
+import * as ff_compiler_Tokenizer from "../../ff/compiler/Tokenizer.mjs"
+
+import * as ff_core_Array from "../../ff/core/Array.mjs"
+
+import * as ff_core_ArrayBuilder from "../../ff/core/ArrayBuilder.mjs"
+
+import * as ff_core_Bool from "../../ff/core/Bool.mjs"
+
+import * as ff_core_BrowserSystem from "../../ff/core/BrowserSystem.mjs"
+
+import * as ff_core_Buffer from "../../ff/core/Buffer.mjs"
+
+import * as ff_core_Channel from "../../ff/core/Channel.mjs"
+
+import * as ff_core_Char from "../../ff/core/Char.mjs"
+
+import * as ff_core_Core from "../../ff/core/Core.mjs"
+
+import * as ff_core_Duration from "../../ff/core/Duration.mjs"
+
+import * as ff_core_Error from "../../ff/core/Error.mjs"
+
+import * as ff_core_FetchSystem from "../../ff/core/FetchSystem.mjs"
+
+import * as ff_core_FileHandle from "../../ff/core/FileHandle.mjs"
+
+import * as ff_core_FileSystem from "../../ff/core/FileSystem.mjs"
+
+import * as ff_core_Float from "../../ff/core/Float.mjs"
+
+import * as ff_core_HttpServer from "../../ff/core/HttpServer.mjs"
+
+import * as ff_core_Instant from "../../ff/core/Instant.mjs"
+
+import * as ff_core_Int from "../../ff/core/Int.mjs"
+
+import * as ff_core_Iterator from "../../ff/core/Iterator.mjs"
+
+import * as ff_core_JsSystem from "../../ff/core/JsSystem.mjs"
+
+import * as ff_core_JsValue from "../../ff/core/JsValue.mjs"
+
+import * as ff_core_List from "../../ff/core/List.mjs"
+
+import * as ff_core_Log from "../../ff/core/Log.mjs"
+
+import * as ff_core_Map from "../../ff/core/Map.mjs"
+
+import * as ff_core_NodeSystem from "../../ff/core/NodeSystem.mjs"
+
+import * as ff_core_Nothing from "../../ff/core/Nothing.mjs"
+
+import * as ff_core_Option from "../../ff/core/Option.mjs"
+
+import * as ff_core_Ordering from "../../ff/core/Ordering.mjs"
+
+import * as ff_core_Pair from "../../ff/core/Pair.mjs"
+
+import * as ff_core_Set from "../../ff/core/Set.mjs"
+
+import * as ff_core_Show from "../../ff/core/Show.mjs"
+
+import * as ff_core_Stream from "../../ff/core/Stream.mjs"
+
+import * as ff_core_String from "../../ff/core/String.mjs"
+
+import * as ff_core_TaskSystem from "../../ff/core/TaskSystem.mjs"
+
+import * as ff_core_TimeSystem from "../../ff/core/TimeSystem.mjs"
+
+import * as ff_core_Try from "../../ff/core/Try.mjs"
+
+import * as ff_core_Unit from "../../ff/core/Unit.mjs"
+
+// type Dependencies
+export function Dependencies(packages_) {
+return {packages_};
+}
+
+
+
+export function process_(fs_, path_) {
+const self_ = ff_compiler_Dependencies.Dependencies(ff_core_List.List_toMap(ff_core_List.Empty(), ff_core_Ordering.ff_core_Ordering_Order$ff_core_Pair_Pair(ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String, ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String)));
+const packageInfo_ = ff_compiler_Dependencies.Dependencies_loadPackageInfo(self_, fs_, ff_core_Pair.Pair("_script", "_script"), path_);
+const newDependencies_ = ff_compiler_Dependencies.Dependencies_processPackageInfo(self_, packageInfo_);
+ff_compiler_Dependencies.Dependencies_processDependencies(self_, fs_, newDependencies_);
+return self_.packages_
+}
+
+export function checkPackagePairs_(dependencyPair_, packagePair_) {
+if(((packagePair_.first_ != dependencyPair_.first_) || (packagePair_.second_ != dependencyPair_.second_))) {
+ff_core_Core.panic_(((((((("Dependency declaration and package declaration disagree on package name: " + dependencyPair_.first_) + ":") + dependencyPair_.second_) + " vs. ") + packagePair_.first_) + ":") + packagePair_.second_))
+}
+}
+
+export async function process_$(fs_, path_, $c) {
+const self_ = ff_compiler_Dependencies.Dependencies(ff_core_List.List_toMap(ff_core_List.Empty(), ff_core_Ordering.ff_core_Ordering_Order$ff_core_Pair_Pair(ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String, ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String)));
+const packageInfo_ = (await ff_compiler_Dependencies.Dependencies_loadPackageInfo$(self_, fs_, ff_core_Pair.Pair("_script", "_script"), path_, $c));
+const newDependencies_ = ff_compiler_Dependencies.Dependencies_processPackageInfo(self_, packageInfo_);
+(await ff_compiler_Dependencies.Dependencies_processDependencies$(self_, fs_, newDependencies_, $c));
+return self_.packages_
+}
+
+export async function checkPackagePairs_$(dependencyPair_, packagePair_, $c) {
+if(((packagePair_.first_ != dependencyPair_.first_) || (packagePair_.second_ != dependencyPair_.second_))) {
+ff_core_Core.panic_(((((((("Dependency declaration and package declaration disagree on package name: " + dependencyPair_.first_) + ":") + dependencyPair_.second_) + " vs. ") + packagePair_.first_) + ":") + packagePair_.second_))
+}
+}
+
+export function Dependencies_loadPackageInfo(self_, fs_, packagePair_, path_) {
+const packageDirectory_ = (ff_core_String.String_endsWith(path_, ".ff")
+? ff_core_FileSystem.directoryName_(path_)
+: path_);
+const sharedPackageFile_ = (packageDirectory_ + "/.firefly/package.ff");
+const packageFile_ = (ff_core_FileSystem.FileSystem_exists(fs_, sharedPackageFile_)
+? sharedPackageFile_
+: path_);
+const code_ = ff_core_FileSystem.FileSystem_readText(fs_, packageFile_);
+const tokens_ = ff_compiler_Tokenizer.tokenize_(packageFile_, code_);
+const parser_ = ff_compiler_Parser.make_(packagePair_, packageFile_, tokens_, false);
+return ff_compiler_Parser.Parser_parsePackageInfo(parser_)
+}
+
+export function Dependencies_processPackageInfo(self_, packageInfo_) {
+self_.packages_ = ff_core_Map.Map_add(self_.packages_, packageInfo_.package_.packagePair_, packageInfo_, ff_core_Ordering.ff_core_Ordering_Order$ff_core_Pair_Pair(ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String, ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String));
+return ff_core_List.List_filter(packageInfo_.dependencies_, ((_w1) => {
+return (!ff_core_Map.Map_contains(self_.packages_, _w1.packagePair_, ff_core_Ordering.ff_core_Ordering_Order$ff_core_Pair_Pair(ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String, ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String)))
+}))
+}
+
+export function Dependencies_fetchDependency(self_, fs_, dependency_) {
+return ff_core_Core.panic_("TODO")
+}
+
+export function Dependencies_processDependencies(self_, fs_, dependencies_) {
+const packageInfos_ = ff_core_List.List_map(dependencies_, ((dependency_) => {
+const path_ = ff_compiler_Dependencies.Dependencies_fetchDependency(self_, fs_, dependency_);
+const packageInfo_ = ff_compiler_Dependencies.Dependencies_loadPackageInfo(self_, fs_, dependency_.packagePair_, path_);
+ff_compiler_Dependencies.checkPackagePairs_(dependency_.packagePair_, packageInfo_.package_.packagePair_);
+return packageInfo_
+}));
+const newDependencies_ = ff_core_List.List_flatMap(packageInfos_, ((_w1) => {
+return ff_compiler_Dependencies.Dependencies_processPackageInfo(self_, _w1)
+}));
+ff_compiler_Dependencies.Dependencies_processDependencies(self_, fs_, newDependencies_)
+}
+
+export async function Dependencies_loadPackageInfo$(self_, fs_, packagePair_, path_, $c) {
+const packageDirectory_ = (ff_core_String.String_endsWith(path_, ".ff")
+? ff_core_FileSystem.directoryName_(path_)
+: path_);
+const sharedPackageFile_ = (packageDirectory_ + "/.firefly/package.ff");
+const packageFile_ = ((await ff_core_FileSystem.FileSystem_exists$(fs_, sharedPackageFile_, $c))
+? sharedPackageFile_
+: path_);
+const code_ = (await ff_core_FileSystem.FileSystem_readText$(fs_, packageFile_, $c));
+const tokens_ = ff_compiler_Tokenizer.tokenize_(packageFile_, code_);
+const parser_ = ff_compiler_Parser.make_(packagePair_, packageFile_, tokens_, false);
+return ff_compiler_Parser.Parser_parsePackageInfo(parser_)
+}
+
+export async function Dependencies_processPackageInfo$(self_, packageInfo_, $c) {
+self_.packages_ = ff_core_Map.Map_add(self_.packages_, packageInfo_.package_.packagePair_, packageInfo_, ff_core_Ordering.ff_core_Ordering_Order$ff_core_Pair_Pair(ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String, ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String));
+return ff_core_List.List_filter(packageInfo_.dependencies_, ((_w1) => {
+return (!ff_core_Map.Map_contains(self_.packages_, _w1.packagePair_, ff_core_Ordering.ff_core_Ordering_Order$ff_core_Pair_Pair(ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String, ff_core_Ordering.ff_core_Ordering_Order$ff_core_String_String)))
+}))
+}
+
+export async function Dependencies_fetchDependency$(self_, fs_, dependency_, $c) {
+return ff_core_Core.panic_("TODO")
+}
+
+export async function Dependencies_processDependencies$(self_, fs_, dependencies_, $c) {
+const packageInfos_ = (await ff_core_List.List_map$(dependencies_, (async (dependency_, $c) => {
+const path_ = (await ff_compiler_Dependencies.Dependencies_fetchDependency$(self_, fs_, dependency_, $c));
+const packageInfo_ = (await ff_compiler_Dependencies.Dependencies_loadPackageInfo$(self_, fs_, dependency_.packagePair_, path_, $c));
+ff_compiler_Dependencies.checkPackagePairs_(dependency_.packagePair_, packageInfo_.package_.packagePair_);
+return packageInfo_
+}), $c));
+const newDependencies_ = ff_core_List.List_flatMap(packageInfos_, ((_w1) => {
+return ff_compiler_Dependencies.Dependencies_processPackageInfo(self_, _w1)
+}));
+(await ff_compiler_Dependencies.Dependencies_processDependencies$(self_, fs_, newDependencies_, $c))
+}
+
+
+
+
