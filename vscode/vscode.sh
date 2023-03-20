1105c0f742a94890f4202fa87f72610acc557542
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2023-03-20 18:51:56 +0100

    Beginnings of a VSCode extension

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -4,4 +4,4 @@ echo "After booting VSCode, press F5 to run a new VSCode window with the extensi
 firefly_sh="$(readlink -f "$0")"
 firefly_vscode="$(dirname "$firefly_sh")"
 firefly="$(readlink -f "$firefly_vscode/..")"
-FIREFLY_HOME="$firefly" code .
+FIREFLY_HOME="$firefly" code "$firefly_vscode"
