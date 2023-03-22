55c0666c478212c9cd8e43e0ff6b471792327438
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2023-03-22 12:27:02 +0100

    VSCode extension

--- vscode/vscode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-echo "After booting VSCode, press F5 to run a new VSCode window with the extension enabled."
-firefly_sh="$(readlink -f "$0")"
-firefly_vscode="$(dirname "$firefly_sh")"
-firefly="$(readlink -f "$firefly_vscode/..")"
-FIREFLY_HOME="$firefly" code "$firefly_vscode"
