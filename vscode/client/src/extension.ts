614a61093cc2434605ae8c2debf1a02a3c952a04
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2023-08-06 17:28:12 +0200

    Published Firefly extension

--- vscode/client/src/extension.ts
+++ vscode/client/src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
         ? path.join(context.extensionPath, 'firefly')
         : path.join(context.extensionPath, '..')
 
-    context.subscriptions.push(vscode.commands.registerCommand('extension.firefly.getFireflyPath', config => {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.firefly-lang.getFireflyPath', config => {
         return fireflyPath;
     }));
 
