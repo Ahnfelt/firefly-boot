08c69cffc2c58e4a8d85d6d76c83717257c0da28
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2024-11-28 18:54:15 +0100

    Fixed internalReadStream

--- output/js/ff/core/Path.mjs
+++ output/js/ff/core/Path.mjs
@@ -182,10 +182,6 @@ break
 }))
 }
 
-export function internalReadStreamOld_(createReadStream_) {
-throw new Error('Function internalReadStreamOld is missing on this target in sync context.');
-}
-
 export async function internalReadStream_$(createReadStream_, $task) {
 let readable_ = ff_core_Option.None();
 let seenError_ = null;
@@ -260,52 +256,6 @@ _w1.destroy()
 break
 }
 }))
-}
-
-export async function internalReadStreamOld_$(createReadStream_, $task) {
-
-        let task = null
-        let readable = null
-        let doResolve = null
-        let doReject = null
-        let seenError = null
-        const abort = () => {
-            if(task != null) {
-                task.controller.signal.removeEventListener('abort', abort)
-                readable.destroy()
-            }
-        }
-        function open($task) {
-            ff_core_Task.Task_throwIfAborted($task)
-            task = $task
-            readable = createReadStream_()
-            readable.on('readable', () => {
-                if(doResolve != null) doResolve()
-            })
-            readable.on('error', error => {
-                task.controller.signal.removeEventListener('abort', abort)
-                seenError = error
-                if(doReject != null) doReject(error)
-            })
-            readable.on('close', () => {
-                task.controller.signal.removeEventListener('abort', abort)
-                if(doResolve != null) doResolve()
-            })
-            $task.controller.signal.addEventListener('abort', abort)
-        }
-        return ff_core_Stream.Stream(async function go($task) {
-            if(task == null) open($task)
-            let buffer = readable.read()
-            if(buffer != null) return ff_core_Option.Some(new DataView(buffer.buffer, buffer.byteOffset, buffer.length))
-            if(seenError != null) throw seenError
-            if(readable.destroyed) return ff_core_Option.None()
-            let promise = new Promise((resolve, reject) => {
-                doResolve = () => {doResolve = null; doReject = null; resolve()}
-                doReject = error => {doResolve = null; doReject = null; reject(error)}
-            }).then(() => go($task))
-            return await promise
-        }, abort)
-    
 }
 
 export function Path_exists(self_, checkReadable_ = false, checkWritable_ = false, checkExecutable_ = false) {
