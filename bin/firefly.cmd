e44617024d7d1046b86fb5de47b78b9925b849be
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2025-11-03 19:18:31 +0100

    JS version of the bin script

--- bin/firefly.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-SETLOCAL
-SET firefly_link=%~f0
-FOR %%I IN ("%firefly_link%") DO SET firefly=%%~dpI\..
-SET NODE_PATH=%firefly%node_modules;%NODE_PATH%
-node --enable-source-maps --harmony-temporal "%firefly%output\js\ff\compiler\Main.run.mjs" %*
