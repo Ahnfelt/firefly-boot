d877db7f0dec22503e73c2ee86c6d49d615a7b69
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2025-11-03 19:59:19 +0100

    JS version of the bin script

--- bin/firefly.js
+++ bin/firefly.js
@@ -3,15 +3,23 @@ const path = require('path');
 const { execSync } = require('child_process');
 
 const fireflyLink = path.resolve(__dirname, '../');
-process.env.NODE_PATH = `${fireflyLink}/node_modules${path.delimiter}${process.env.NODE_PATH || ''}`;
+const env = {
+    ...process.env,
+    NODE_PATH: `${path.join(fireflyLink, 'node_modules')}${path.delimiter}${process.env.NODE_PATH || ''}`,
+}
 
 const args = process.argv.slice(2);
 const compilerPath = path.join(fireflyLink, 'output/js/ff/compiler/Main.run.mjs');
 
 try {
-    execSync(`node --enable-source-maps --harmony-temporal "${compilerPath}" ${args.join(' ')}`, {
+    execSync('node', [
+        '--enable-source-maps',
+        '--harmony-temporal',
+        compilerPath,
+        ...args
+    ], {
         stdio: 'inherit',
-        env: process.env,
+        env: env,
     });
 } catch (error) {
     process.exit(error.status || 1);
